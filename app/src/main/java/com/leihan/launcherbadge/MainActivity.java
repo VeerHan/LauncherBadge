c852e9dca82ee993f537560dc1d392cbc94e1980
Leo.Han <leoleohan@foxmail.com>
2016-08-24 16:49:19 +0800

    add samsung badge

--- app/src/main/java/com/leihan/launcherbadge/MainActivity.java
+++ app/src/main/java/com/leihan/launcherbadge/MainActivity.java
@@ -1,17 +1,22 @@
 package com.leihan.launcherbadge;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import java.util.List;
 
 import me.leolin.shortcutbadger.ShortcutBadger;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private int badgeCount = 6;
-    private Button btnAdd, btnRemove;
+    private int badgeCount;//待显示的角标数量
+    private Button btnAdd, btnRemove, btnPartAdd, btnPartRemove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +24,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         btnAdd = (Button) findViewById(R.id.btn_add);
         btnRemove = (Button) findViewById(R.id.btn_remove);
+        btnPartAdd = (Button) findViewById(R.id.btn_custom_add);
+        btnPartRemove = (Button) findViewById(R.id.btn_custom_remove);
         btnAdd.setOnClickListener(this);
         btnRemove.setOnClickListener(this);
+        btnPartAdd.setOnClickListener(this);
+        btnPartRemove.setOnClickListener(this);
     }
 
     @Override
@@ -32,6 +41,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_remove:
                 ShortcutBadger.removeCount(this);
                 break;
+            case R.id.btn_custom_add:
+                badgeCount = 8;
+                if (DeviceUtil.isSpecialDevice(DeviceUtil.DEVICE_SAMSUNG)) {
+                    setSamsungBadge(this, badgeCount);
+                }
+                break;
+            case R.id.btn_custom_remove:
+                badgeCount = 0;
+                if (DeviceUtil.isSpecialDevice(DeviceUtil.DEVICE_SAMSUNG)) {
+                    setSamsungBadge(this, badgeCount);
+                }
+                break;
         }
+    }
+
+    /**
+     * 设置三星角标
+     *
+     * @param context
+     * @param count
+     */
+    public static void setSamsungBadge(Context context, int count) {
+        String launcherClassName = getLauncherClassName(context);
+        if (launcherClassName == null) {
+            return;
+        }
+        Intent intent = new Intent("android.intent.action.BADGE_COUNT_UPDATE");
+        intent.putExtra("badge_count", count);
+        intent.putExtra("badge_count_package_name", context.getPackageName());
+        intent.putExtra("badge_count_class_name", launcherClassName);
+        context.sendBroadcast(intent);
+    }
+
+    public static String getLauncherClassName(Context context) {
+        PackageManager pm = context.getPackageManager();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intent, 0);
+        for (ResolveInfo resolveInfo : resolveInfos) {
+            String pkgName = resolveInfo.activityInfo.applicationInfo.packageName;
+            if (pkgName.equalsIgnoreCase(context.getPackageName())) {
+                String className = resolveInfo.activityInfo.name;
+                return className;
+            }
+        }
+        return null;
     }
 }
